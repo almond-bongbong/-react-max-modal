8c8b30a60d663e9c05f9916a021a7f81654581de
cmlee <bal.dongdongdong@gmail.com>
2020-08-27 00:28:18 +0900

    default animation develop

--- example/src/App.js
+++ example/src/App.js
@@ -2,16 +2,20 @@ import React, { useState } from 'react'
 import Modal from '@max/modal'
 
 function App() {
-  const [show, setShow] = useState(false);
+  const [visible, setVisible] = useState(false);
 
-  const handleModal = () => {
-    setShow(true);
+  const openModal = () => {
+    setVisible(true);
   }
+
+  const closeModal = () => {
+    setVisible(false);
+  };
 
   return (
     <div>
-      <button type="button" onClick={handleModal}>Open modal</button>
-      <Modal show={show}>
+      <button type="button" onClick={openModal}>Open modal</button>
+      <Modal visible={visible} onClose={closeModal}>
         Hello, Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam aliquid doloremque eum illum magnam maiores neque pariatur quis quisquam quod quos, rerum sint, unde. Animi expedita fuga odio quae temporibus.
       </Modal>
     </div>
--- src/index.tsx
+++ src/index.tsx
@@ -1,18 +1,64 @@
 import * as React from 'react';
-import { ReactElement, ReactNode } from 'react';
+import { ReactElement, ReactNode, useCallback, useEffect, useState } from 'react';
 import Portal from './components/Portal';
+import styles from './styles.css';
 
 interface Props {
-  show: boolean;
+  visible: boolean;
   children: ReactNode;
+  onClose: () => void;
+  maskClosable?: boolean;
+  escKeyClosable?: boolean;
 }
 
-function Modal({ show, children }: Props): ReactElement {
+function Modal({
+  visible,
+  children,
+  onClose,
+  maskClosable = true,
+  escKeyClosable = true,
+}: Props): ReactElement {
+  const [localVisible, setLocalVisible] = useState(visible);
+
+  useEffect(() => {
+    if (visible) {
+      setLocalVisible(visible);
+    } else {
+      setTimeout(() => setLocalVisible(visible), 300);
+    }
+  }, [visible]);
+
+  const keydownHandler = useCallback((e) => {
+    if (e.code === 'Escape') {
+      onClose?.();
+    }
+  }, [onClose]);
+
+  useEffect(() => {
+    if (escKeyClosable) document.addEventListener('keydown', keydownHandler);
+    return () => {
+      document.removeEventListener('keydown', keydownHandler);
+    };
+  }, [
+    escKeyClosable,
+    keydownHandler,
+  ]);
+
+  const handleClickMask = useCallback(() => {
+    if (maskClosable) onClose?.();
+  }, [maskClosable, onClose]);
+
+  const modalClassNames = [
+    styles.modal,
+    visible && localVisible && styles.active,
+  ].filter(Boolean).join(' ');
+
   return (
     <Portal>
-      {show && (
-        <div>
-          {children}
+      {(visible || (!visible && localVisible)) && (
+        <div className={modalClassNames}>
+          <div className={styles.mask} onClick={handleClickMask} />
+          <div className={styles.content}>{children}</div>
         </div>
       )}
     </Portal>
