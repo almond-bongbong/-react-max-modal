3b0c64d195ff48cd01c04ee0f8441dbfc14153fc
cmlee <bal.dongdongdong@gmail.com>
2020-09-08 01:41:12 +0900

    size reducing

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,6 @@
 import typescript from 'rollup-plugin-typescript2'
 import commonjs from 'rollup-plugin-commonjs'
 import external from 'rollup-plugin-peer-deps-external'
-// import postcss from 'rollup-plugin-postcss-modules'
 import postcss from 'rollup-plugin-postcss'
 import resolve from 'rollup-plugin-node-resolve'
 import url from 'rollup-plugin-url'
@@ -14,15 +13,9 @@ export default {
   output: [
     {
       file: pkg.main,
-      format: 'cjs',
-      exports: 'named',
-      sourcemap: true
-    },
-    {
-      file: pkg.module,
       format: 'es',
       exports: 'named',
-      sourcemap: true
+      sourcemap: false
     }
   ],
   plugins: [
@@ -35,7 +28,7 @@ export default {
     resolve(),
     typescript({
       rollupCommonJSResolveHack: true,
-      clean: true
+      clean: true,
     }),
     commonjs()
   ]
